7d83606e19edc470c639f773f82ab9c50952b2b6
Subhasis Majee <subhasis.majee03@gmail.com>
2023-07-19 21:57:28 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    
-
     return MaterialApp(
-      home:HomePage(), 
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(primarySwatch: Colors.green),
+      darkTheme: ThemeData(
+        // brightness: Brightness.dark,
+        primarySwatch: Colors.purple,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
