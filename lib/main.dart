e93070dec28e77af7e1cf83cc9e5767ddf528eb7
Subhasis Majee <subhasis.majee03@gmail.com>
2023-07-19 12:10:33 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+          child: Center(
+              child: Container(
+        child: Text("welcome to my first flutter app"),
+      ))),
+    );
+  }
+}
