03961a27bda9955300d377afdfdfb5fd9c47d416
Subhasis Majee <subhasis.majee03@gmail.com>
2023-08-04 22:51:35 +0530

    day15 completed

--- lib/widgets/item_widget.dart
+++ lib/widgets/item_widget.dart
@@ -10,7 +10,7 @@ class ItemWidget extends StatelessWidget {
     return Card(
       child: ListTile(
         onTap: () {
-          // print("${item.name} pressed");
+          print("${item.name} pressed");
         },
         leading: Image.network(item.image),
         title: Text(item.name),
