d943efaed0162c2fe8be39e24a193fe699faa4c2
Subhasis Majee <subhasis.majee03@gmail.com>
2023-08-09 21:22:31 +0530

    day18 competed

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
@@ -9,10 +10,72 @@ class CartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: context.canvasColor,
-      
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         title: "Cart".text.make(),
+      ),
+      body: Column(
+        children: [
+          cartList().p32().expand(),
+          Divider(),
+          cartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class cartTotal extends StatelessWidget {
+  const cartTotal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\u{20B9}${9999}"
+              .text
+              .xl4
+              .color(context.theme.colorScheme.secondary)
+              .make(),
+          30.widthBox,
+          ElevatedButton(
+            onPressed: () {},
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(
+              context.theme.highlightColor,
+            )),
+            child: "Buy".text.white.make(),
+          ).w24(context)
+        ],
+      ),
+    );
+  }
+}
+
+class cartList extends StatefulWidget {
+  const cartList({super.key});
+
+  @override
+  State<cartList> createState() => _cartListState();
+}
+
+class _cartListState extends State<cartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        leading: const Icon(Icons.done),
+        trailing: IconButton(
+          icon: const Icon(
+            Icons.remove_circle_outline,
+          ),
+          onPressed: () {},
+        ),
+        title: "Item 1".text.make(),
       ),
     );
   }
