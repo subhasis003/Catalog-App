3f9a003c57c75c002f51cb1ae5f94820cd550cc0
Subhasis Majee <subhasis.majee03@gmail.com>
2023-08-12 22:05:23 +0530

    in progress

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/models/cart.dart';
-import 'package:flutter_catalog/widgets/themes.dart';
+// import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class CartPage extends StatelessWidget {
