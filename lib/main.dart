98fcf0493a03cff1327454ad4a18dfe8a5658116
Subhasis Majee <subhasis.majee03@gmail.com>
2023-09-19 00:21:27 +0530

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/core/store.dart';
 import 'package:flutter_catalog/pages/cart_page.dart';
@@ -7,8 +7,11 @@ import 'package:flutter_catalog/pages/login_page.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
+// import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     VxState(
       store: MyStore(),
@@ -25,9 +28,10 @@ class MyApp extends StatelessWidget {
     // TextTheme textTheme = Theme.of(context).textTheme;
     return MaterialApp(
       themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
       theme: MyTheme.lightTheme(context),
       darkTheme: MyTheme.darkTheme(context),
-      initialRoute: MyRoutes.homeRoute,
+      initialRoute: MyRoutes.loginRoute,
       routes: {
         "/": (context) => const LoginPage(),
         MyRoutes.homeRoute: (context) => const HomePage(),
